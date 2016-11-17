ec6d0214f782cdc920249d72dec6ac4a611e605f
SaladRaider <peterkangveerman@gmail.com>
2016-11-16 17:39:42 -0800

    Added hello world test for server extension.

--- nbinteract/__init__.py
+++ nbinteract/__init__.py
@@ -14,5 +14,18 @@ def _jupyter_nbextension_paths():
         "require": "nbinteract/main"
     }]
 
+class HelloWorldHandler(IPythonHandler):
+    def get(self):
+        self.finish('Hello, world!')
+
 def load_jupyter_server_extension(nbapp):
-    pass
+    """
+    Called when the extension is loaded.
+
+    Args:
+        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
+    """
+    web_app = nbapp.web_app
+    host_pattern = '.*$'
+    route_pattern = url_path_join(web_app.settings['base_url'], '/hello')
+    web_app.add_handlers(host_pattern, [(route_pattern, HelloWorldHandler)])
