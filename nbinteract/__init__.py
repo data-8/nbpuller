fac9759916bee71252515c8ee6713a5e82fb3614
SaladRaider <peterkangveerman@gmail.com>
2016-11-17 20:29:22 -0800

    Added dependencies to Dockerfile. Fixed host pattern.

--- nbinteract/__init__.py
+++ nbinteract/__init__.py
@@ -15,13 +15,7 @@ def _jupyter_nbextension_paths():
     }]
 
 def load_jupyter_server_extension(nbapp):
-    """
-    Called when the extension is loaded.
-
-    Args:
-        nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
-    """
     web_app = nbapp.web_app
-    host_pattern = '.*$'
+    host_pattern = '.*'
     route_pattern = url_path_join(web_app.settings['base_url'], '/interact')
     web_app.add_handlers(host_pattern, [(route_pattern, LandingHandler)])
