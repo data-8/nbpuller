bac6e3f1d2c23e90c386c3bfe53e547ac6740e7e
SaladRaider <peterkangveerman@gmail.com>
2016-12-01 20:43:44 -0800

    Bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="nbinteract",
-    version='0.0.7',
+    version='0.0.8',
     url="https://github.com/data-8/nbinteract",
     author="Data 8",
     description="Simple Jupyter extension to use interact.",
